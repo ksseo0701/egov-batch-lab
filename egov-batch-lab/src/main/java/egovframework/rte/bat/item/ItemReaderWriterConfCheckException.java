36d37317dd875b77fb609e16d4b85af9ff8c2d2e
Seo Kyung-Seok <ksseo0701@gmail.com>
2014-11-02 20:45:40 +0900

    Default Reader/Writer Create

---
+++ egov-batch-lab/src/main/java/egovframework/rte/bat/item/ItemReaderWriterConfCheckException.java
@@ -0,0 +1,15 @@
+package egovframework.rte.bat.item;
+
+public class ItemReaderWriterConfCheckException extends RuntimeException {
+
+    public ItemReaderWriterConfCheckException( String message )
+    {
+        super( message );
+    }
+
+    public ItemReaderWriterConfCheckException( String message, Throwable cause )
+    {
+        super( message, cause );
+    }
+
+}
